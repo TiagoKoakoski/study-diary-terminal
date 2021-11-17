0d46fdbbcf74e3c582fdb04b2db632b1433133f2
Tiago Koakoski <tiago.koakoski@gmail.com>
2021-11-17 11:39:40 -0300

    v0.3

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -1,16 +1,28 @@
+CADASTRAR = 1
+LISTAR = 2
+BUSCAR = 3
+REMOVER = 4
+CONCLUIR = 5
 SAIR = 9
 require_relative 'study_item'
 
 def menu()
-  puts "[1] Cadastrar um item para estudar"
-  puts "[2] Ver todos os itens cadastrados"
-  puts "[3] Buscar um item de estudo"
-  puts "[4] Remover item"
-  puts "[5] Busca avançada"
+  puts "[#{CADASTRAR}] Cadastrar um item para estudar"
+  puts "[#{LISTAR}] Ver todos os itens cadastrados"
+  puts "[#{BUSCAR}] Buscar um item de estudo"
+  puts "[#{REMOVER}] Remover item"
+  puts "[#{CONCLUIR}] Concluir item"
   puts "[6] Listar por categoria"
+  puts "[7] Ver os itens concluídos"
   puts "[#{SAIR}] Sair"
   print "Escolha uma opção: "
   gets.to_i
+end
+
+def listar_itens()
+  puts "Lista de itens cadastrados: "
+  estudos = StudyItem.find_undone
+  estudos.each_with_index {|valor, ind| puts "[#{ind+1}] - #{valor.title} - #{valor.description} - Categoria: #{valor.category}"}
 end
 
 def selecionar_categoria()
@@ -35,57 +47,65 @@ puts "Bem-vindo ao Diário de Estudos, seu companheiro de estudos"
 
 while opcao != SAIR
   opcao = menu()
+  system "clear"
   case opcao
-  when 1
+  when CADASTRAR
     system "clear"
-    puts "Digite o item para estudo: "
-    item = gets.chomp()
+    print "Digite o item para estudo: "
+    item = gets.chomp().capitalize
     categoria = selecionar_categoria()
-    estudos = StudyItem.new(title: item, category: categoria)
+    print "Digite a descrição do item para estudo: "
+    descricao = gets.chomp().capitalize
+    estudos = StudyItem.new(title: item, category: categoria, description: descricao)
     estudos.save_to_db
     opcao = 0
     puts "Item #{item} cadastrado com sucesso na categoria #{categoria}"
     gets
+
+  when LISTAR
     system "clear"
-  when 2
-    system "clear"
-    puts "Lista de itens cadastrados: "
-    estudos = StudyItem.all
-    estudos.each{|valor| puts "Item #{valor.title} na categoria #{valor.category}"}
-    opcao = 0
+    listar_itens()
     gets
-    system "clear"
-  when 3
-    system "clear"
-    puts "Qual item deseja encontrar: "
-    busca = gets.chomp()
-    puts "Itens encontrados: "
-    busca = StudyItem.find_by_title(busca)
-    busca.each{|valor| puts (valor.title + " / " + valor.category)}
-    opcao = 0
-    gets
-    system "clear"
-  when 4
-    puts "Qual o titulo do item que deseja remover: "
-    busca = gets.chomp
-    StudyItem.delete(busca)
-  when 5
+
+  when BUSCAR
     system "clear"
     puts "Qual item deseja encontrar: "
     busca = gets.chomp
     estudos = StudyItem.all
     estudos.map do |valor|
-       if valor.title.include? busca
-         puts "Item #{valor.title} na categoria #{valor.category}"
+       if (valor.title.include? busca) || (valor.description.include? busca)
+         puts " - #{valor.title} - #{valor.description} - Categoria #{valor.category}"
        end
      end
      gets
-     system "clear"
+
+  when REMOVER
+    lista = listar_itens()
+    print "Qual o item que deseja remover: "
+    selecao = gets.to_i
+    selecao = lista[(selecao - 1)].title
+    StudyItem.delete(selecao)
+    puts "item removido com sucesso"
+    gets
+
+  when CONCLUIR
+    lista = listar_itens()
+    print "Qual o item que deseja concluir: "
+    selecao = gets.to_i
+    selecao = lista[(selecao - 1)].title
+    StudyItem.done(selecao)
+    puts "item concluído com sucesso"
+    gets
+
   when 6
     categoria = selecionar_categoria()
     StudyItem.find_by_category(categoria)
     gets
-    system "clear"
+
+  when 7
+    StudyItem.find_done()
+    gets
+
   when SAIR
     system "clear"
     puts "Obrigado por utilizar o Diário de Estudos"
--- lib/study_item.rb
+++ lib/study_item.rb
@@ -2,24 +2,42 @@ require 'sqlite3'
 require_relative 'category'
 
 class StudyItem
-  attr_accessor :title, :category
+  attr_accessor :title, :category, :description, :done
 
-  def initialize ( title:, category: Category.new )
+  def initialize ( title:, category: Category.new, description: "Não preenchido" )
     @title = title
     @category = category
+    @description = description
+    @done = 0
+  end
+
+  def selecionar_categoria()
+    puts "Qual a categoria:
+    1 - Ruby
+    2 - Rails
+    3 - HTML"
+    categoria = gets.to_i()
+    if categoria == 1
+      categoria = "Ruby"
+    elsif categoria == 2
+      categoria = "Rails"
+    else
+      categoria = "HTML"
+    end
+    categoria
   end
 
   def self.all
     db = SQLite3::Database.open "db/database.db"
     db.results_as_hash = true
-    itens = db.execute "SELECT title, category FROM diario"
+    itens = db.execute "SELECT title, category, description FROM diario"
     db.close
-    itens.map {|item| new(title: item['title'], category: item['category']) }
+    itens.map {|item| new(title: item['title'], category: item['category'], description: item['description']) }
   end
 
   def save_to_db
     db = SQLite3::Database.open "db/database.db"
-    db.execute "INSERT INTO diario VALUES('#{ title }', '#{ category }')"
+    db.execute "INSERT INTO diario VALUES('#{ title }', '#{ category }', '#{ description }', 0)"
     db.close
     self
   end
@@ -34,13 +52,35 @@ class StudyItem
 
   def self.find_by_category(categoria)
     system "clear"
-        puts "Os itens na categoria #{categoria} são: "
+    puts "Os itens na categoria #{categoria} são: "
     db = SQLite3::Database.open "db/database.db"
     db.results_as_hash = true
-    itens = db.execute "SELECT title, category FROM diario where category='#{categoria}'"
+    itens = db.execute "SELECT title, category, description FROM diario where category='#{categoria}'"
     db.close
     lista = itens.map {|item| new(title: item['title'], category: item['category']) }
-    lista.each{|valor| puts "Item #{valor.title}"}
+    lista.each{|valor| puts " - #{valor.title} - #{valor.description}"}
+  end
+
+  def self.find_undone()
+    system "clear"
+    puts "Os itens não concluídos são: "
+    db = SQLite3::Database.open "db/database.db"
+    db.results_as_hash = true
+    itens = db.execute "SELECT title, category, description FROM diario where done='0'"
+    db.close
+    lista = itens.map {|item| new(title: item['title'], category: item['category'], description: item['description']) }
+    #lista.each{|valor| puts " - #{valor.title} - #{valor.description}"}
+  end
+
+  def self.find_done()
+    system "clear"
+    puts "Os itens concluídos são: "
+    db = SQLite3::Database.open "db/database.db"
+    db.results_as_hash = true
+    itens = db.execute "SELECT title, category, description FROM diario where done='1'"
+    db.close
+    lista = itens.map {|item| new(title: item['title'], category: item['category'], description: item['description']) }
+    lista.each{|valor| puts " - #{valor.title} - #{valor.description}"}
   end
 
   def self.delete(title)
@@ -49,6 +89,11 @@ class StudyItem
     db.close
   end
 
+  def self.done(title)
+    db = SQLite3::Database.open "db/database.db"
+    db.execute "UPDATE diario SET done='1' WHERE title='#{ title }'"
+    db.close
+  end
 
 
 end
