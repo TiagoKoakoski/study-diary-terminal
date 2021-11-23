fd67173800380922a0cdfa6cbc2edfa5f43f86a4
Tiago Koakoski <tiago.koakoski@gmail.com>
2021-11-23 14:57:08 -0300

    v1.4

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -28,94 +28,16 @@ def menu
   gets.to_i
 end
 
-def select_category()
-  puts "Qual a categoria:
-  1 - Ruby
-  2 - Rails
-  3 - HTML"
-  print "Categoria: ".yellow
-  category = gets.to_i()
-  if category == 1
-    category = "Ruby"
-  elsif category == 2
-    category = "Rails"
-  elsif category == 3
-    category = "HTML"
-  end
-  category
-end
-
-def create_study_item
-  print "Digite o item para estudo: ".yellow
-  item = gets.chomp()
-  category = select_category()
-  print "Digite a descrição do item para estudo: ".yellow
-  description = gets.chomp()
-  study_item = StudyItem.new(title: item, category: category, description: description)
-  study_item.save_to_db
-  option = 0
-  puts "Item #{item} cadastrado com sucesso na categoria #{category}"
-end
-
-
 def list_items()
   puts "Lista de itens cadastrados: "
   study_items = StudyItem.find_undone
   study_items.each_with_index {|valor, ind| puts "[#{ind+1}] - #{valor.title} - #{valor.description} - Categoria: #{valor.category}"}
 end
 
-def search_item()
-  print "Qual item deseja encontrar: ".yellow
-  search = gets.chomp
-  study_list = StudyItem.all
-  puts "Itens encontrados: "
-  study_list.each do |valor|
-    if ( valor.title.include? search ) || ( valor.description.include? search )
-      puts " - #{valor.title} - #{valor.description} - Categoria #{valor.category}"
-    end
-  end
-end
-
-def delete_item()
-  list = list_items()
-  print "Qual o item que deseja remover: ".yellow
-  select = gets.chomp
-  return if select.empty?
-
-  select = select.to_i
-  select = list[(select - 1)].title
-  StudyItem.delete(select)
-  puts "item removido com sucesso"
-end
-
-def make_done()
-  list = list_items()
-  print "Qual o item que deseja concluir: ".yellow
-  select = gets.chomp
-  return if select.empty?
-
-  select = select.to_i
-  select = list[(select - 1)].title
-  StudyItem.done(select)
-  puts "item concluído com sucesso"
-end
-
 def list_done()
   puts "Lista de itens concluídos: "
   study_items = StudyItem.find_done
   study_items.each_with_index {|valor, ind| puts "[#{ind+1}] - #{valor.title} - #{valor.description} - Categoria: #{valor.category}"}
-end
-
-def make_undone()
-  list = list_done()
-  print "Qual o item que deseja voltar a estudar: ".yellow
-  select = gets.chomp
-  return if select.empty?
-
-  select = select.to_i
-  select = list[(select - 1)].title
-  StudyItem.undone(select)
-  puts "item retornou para estudo com sucesso"
 end
 
 def fim_de_funcao()
@@ -134,42 +56,32 @@ while option != EXIT
   system "clear"
   case option
   when INSERT
-    create_study_item()
+    StudyItem.create_study_item()
     fim_de_funcao()
-
   when VIEW
     list_items()
     fim_de_funcao()
-
   when SEARCH
-    search_item()
+    StudyItem.search_item()
     fim_de_funcao()
-
   when DELETE
-    delete_item()
+    StudyItem.delete()
     fim_de_funcao()
-
   when DONE
-    make_done()
+    StudyItem.done()
     fim_de_funcao()
-
   when LIST_CATEGORY
-    categoria = select_category()
-    StudyItem.find_by_category(categoria)
+    StudyItem.find_by_category()
     fim_de_funcao()
-
   when LIST_DONE
     list_done()
     fim_de_funcao()
-
   when UNDONE
-    make_undone()
+    StudyItem.undone()
     fim_de_funcao()
-
   when EXIT
     system "clear"
     puts "Obrigado por utilizar o Diário de Estudos"
-
   else
     puts 'Opção inválida'
   end
--- lib/study_item.rb
+++ lib/study_item.rb
@@ -11,20 +11,21 @@ class StudyItem
     @done = done
   end
 
-  def selecionar_categoria()
+  def self.select_category()
     puts "Qual a categoria:
     1 - Ruby
     2 - Rails
     3 - HTML"
-    categoria = gets.to_i()
-    if categoria == 1
-      categoria = "Ruby"
-    elsif categoria == 2
-      categoria = "Rails"
-    else
-      categoria = "HTML"
+    print "Categoria: ".yellow
+    category = gets.to_i()
+    if category == 1
+      category = "Ruby"
+    elsif category == 2
+      category = "Rails"
+    elsif category == 3
+      category = "HTML"
     end
-    categoria
+    category
   end
 
   def self.all
@@ -42,12 +43,37 @@ class StudyItem
     self
   end
 
-  def self.find_by_category(categoria)
+  def self.search_item()
+    print "Qual item deseja encontrar: ".yellow
+    search = gets.chomp
+    study_list = StudyItem.all
+    puts "Itens encontrados: "
+    study_list.each do |valor|
+      if ( valor.title.include? search ) || ( valor.description.include? search )
+        puts " - #{valor.title} - #{valor.description} - Categoria #{valor.category}"
+      end
+    end
+  end
+
+  def self.create_study_item
+    print "Digite o item para estudo: ".yellow
+    item = gets.chomp()
+    category = self.select_category()
+    print "Digite a descrição do item para estudo: ".yellow
+    description = gets.chomp()
+    study_item = StudyItem.new(title: item, category: category, description: description)
+    study_item.save_to_db
+    option = 0
+    puts "Item #{item} cadastrado com sucesso na categoria #{category}"
+  end
+
+  def self.find_by_category()
     system "clear"
-    puts "Os itens na categoria #{categoria} são: "
+    category = select_category()
+    puts "Os itens na categoria #{category} são: "
     db = SQLite3::Database.open "db/database.db"
     db.results_as_hash = true
-    itens = db.execute "SELECT title, category, description, done FROM diario where category='#{categoria}'"
+    itens = db.execute "SELECT title, category, description, done FROM diario where category='#{category}'"
     db.close
     lista = itens.map {|item| new(title: item['title'], category: item['category'], description: item['description'], done: item['done']) }
     lista.each{|valor| puts " - #{valor.title} - #{valor.description} #{'- Concluído' if valor.done == 1}"}
@@ -73,22 +99,43 @@ class StudyItem
     lista = itens.map {|item| new(title: item['title'], category: item['category'], description: item['description']) }
   end
 
-  def self.delete(title)
+  def self.delete()
+    list = list_items()
+    print "Qual o item que deseja remover: ".yellow
+    select = gets.chomp
+    return if select.empty?
+    select = select.to_i
+    select = list[(select - 1)].title
     db = SQLite3::Database.open "db/database.db"
-    db.execute "DELETE FROM diario WHERE title='#{ title }'"
+    db.execute "DELETE FROM diario WHERE title='#{ select }'"
     db.close
+    puts "item removido com sucesso"
   end
 
-  def self.done(title)
+  def self.done()
+    list = list_items()
+    print "Qual o item que deseja concluir: ".yellow
+    select = gets.chomp
+    return if select.empty?
+    select = select.to_i
+    select = list[(select - 1)].title
     db = SQLite3::Database.open "db/database.db"
-    db.execute "UPDATE diario SET done='1' WHERE title='#{ title }'"
+    db.execute "UPDATE diario SET done='1' WHERE title='#{ select }'"
     db.close
+    puts "item concluído com sucesso"
   end
 
-  def self.undone(title)
+  def self.undone()
+    list = list_done()
+    print "Qual o item que deseja voltar a estudar: ".yellow
+    select = gets.chomp
+    return if select.empty?
+    select = select.to_i
+    select = list[(select - 1)].title
     db = SQLite3::Database.open "db/database.db"
-    db.execute "UPDATE diario SET done='0' WHERE title='#{ title }'"
+    db.execute "UPDATE diario SET done='0' WHERE title='#{ select }'"
     db.close
+    puts "item retornou para estudo com sucesso"
   end
 
 end
